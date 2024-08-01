a76b401b4c8f5749716b10edfc37f3da8d57ad72
Zikkamen <jeffreyntue@gmail.com>
2024-08-01 13:31:58 +0200

    Added Parsers and integrated database into the websocket

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,17 @@ mod values_store;
 mod web_clients;
 mod file_reader;
 mod database_clients;
+mod data_parsers;
 
 use crate::values_store::credentials_store::CredentialsStore;
 use crate::web_clients::finnhub::FinnhubClient;
 use crate::database_clients::postgres_client::PostgresClient;
+use crate::data_parsers::finnhub_parser::parse_finnhub_data;
 
 fn main() {
     let credentials_store:CredentialsStore = CredentialsStore::new();
+    let mut postgres_client:PostgresClient = PostgresClient::new();
 
-    let finnhub_client:FinnhubClient = FinnhubClient::new(credentials_store);
+    let mut finnhub_client:FinnhubClient = FinnhubClient::new(credentials_store, postgres_client);
     finnhub_client.print_hello();
-
-    //let mut postgres_client:PostgresClient = PostgresClient::new();
-    //postgres_client.print_hello();
 }
